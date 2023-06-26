dac287152d28875d29382a8f52619e9ff94585d2
Adithya R <rsadithya12@gmail.com>
2023-06-26 14:05:03 +0530

    somethig

--- Frontend/educonnect/components/Footer.component.jsx
+++ Frontend/educonnect/components/Footer.component.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-gradient-to-br bg-gray-900 bg-white bg-gray-900 z-50">
+    <footer className="bg-gradient-to-br z-50">
       <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8 z-40">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
--- Frontend/educonnect/components/sidebar.component.jsx
+++ Frontend/educonnect/components/sidebar.component.jsx
@@ -117,9 +117,7 @@ const ModuleBar = () => {
               </li>
             ))}
             <li>
-              
                 <button type="button" onClick={()=>push('/courses')} className="w-full text-white bg-violet-500 hover:bg-blue-500 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2  focus:outline-none ">Go Back</button>
-              
             </li>
           </ul>
         </div>
