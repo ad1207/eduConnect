22a7526ee20c822b0a1e7e3cc7d63307f1b32340
Reuben-Stephen-John <reuben.sj2003@gmail.com>
2023-06-26 10:39:59 +0530

    createpagebrowser

--- Frontend/educonnect/pages/_app.js
+++ Frontend/educonnect/pages/_app.js
@@ -1,9 +1,9 @@
 import '@/styles/globals.css'
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs'
 import {SessionContextProvider} from '@supabase/auth-helpers-react'
 import { useState } from 'react'
 export default function App({ Component, pageProps }) {
-  const [supabase] = useState(() => createBrowserSupabaseClient())
+  const [supabase] = useState(() => createPagesBrowserClient())
   
   return (
     <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
