c3e7248076a8432c7e753e40f8254f6895795c08
Reuben-Stephen-John <reuben.sj2003@gmail.com>
2023-06-26 15:36:36 +0530

    read aloud features added

--- Frontend/educonnect/components/displayData.component.jsx
+++ Frontend/educonnect/components/displayData.component.jsx
@@ -1,16 +1,66 @@
-import axios from "axios";
-import uuidv4 from "uuidv4";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import uuidv4 from 'uuidv4';
+import svg from '../public/speaker.svg';
+import Image from 'next/image';
 
 const DisplayData = ({ moduleId, modulesData, sample }) => {
+  const [speechSynthesizer, setSpeechSynthesizer] = useState(null);
+  const [isPlaying, setIsPlaying] = useState(false);
   const selectedModule = modulesData.find((module) => module.id == moduleId);
-  console.log("module data", modulesData, moduleId, sample);
+  console.log('module data', modulesData, moduleId, sample);
 
-  
+  const synthesizeSpeech = (text) => {
+    const sdk = require('microsoft-cognitiveservices-speech-sdk');
+    const speechConfig = sdk.SpeechConfig.fromSubscription(
+      'd7c19d75fd5046b38a6b1bd891e13dcb',
+      'eastus'
+    );
+    const audioConfig = sdk.AudioConfig.fromDefaultSpeakerOutput();
+    
+
+    const newSpeechSynthesizer = new sdk.SpeechSynthesizer(speechConfig, audioConfig);
+    setSpeechSynthesizer(newSpeechSynthesizer);
+    setIsPlaying(true);
+
+    newSpeechSynthesizer.speakTextAsync(
+      text,
+      (result) => {
+        if (result) {
+          newSpeechSynthesizer.close();
+          setSpeechSynthesizer(null);
+          setIsPlaying(false);
+          return result.audioData;
+        }
+      },
+      (error) => {
+        console.log(error);
+        newSpeechSynthesizer.close();
+        setSpeechSynthesizer(null);
+        setIsPlaying(false);
+      }
+    );
+  };
+
+  const handleSynthesizeSpeech = () => {
+    if (selectedModule && selectedModule.notes) {
+      synthesizeSpeech(selectedModule.notes);
+    }
+  };
+
+  useEffect(() => {
+    // Cleanup function to stop audio when unmounting the component
+    return () => {
+      if (speechSynthesizer) {
+        speechSynthesizer.close();
+        setSpeechSynthesizer(null);
+      }
+    };
+  }, [speechSynthesizer]);
 
   if (!selectedModule) {
     return <div>No module selected</div>;
   }
-  // getLanguages()
 
   return (
     <>
@@ -18,6 +68,19 @@ const DisplayData = ({ moduleId, modulesData, sample }) => {
         <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-6 lg:px-6">
           <div className="max-w-screen-lg text-gray-500 sm:text-lg ">
             <div>
+            {modulesData.length > 0 && (
+                  <button
+                  onClick={handleSynthesizeSpeech}
+                  disabled={isPlaying}
+                  className={`absolute right-12 p-2 flex items-center text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ${
+                    isPlaying ? 'opacity-50 cursor-not-allowed' : ''
+                  }`}
+                >
+                  {isPlaying ? 'Playing...' : 'Read Aloud'}
+                  <Image src={svg} alt="Speaker Icon" className="w-5 h-5 ml-2" />
+                </button>
+                  
+                )}
               <h3 className="text-2xl font-semibold">
                 {selectedModule.module_name}
               </h3>
--- Frontend/educonnect/pages/courses/[courseId]/index.js
+++ Frontend/educonnect/pages/courses/[courseId]/index.js
@@ -13,33 +13,34 @@ const CoursesPage = ({data}) => {
 export default CoursesPage;
 
 
-export async function getServerSideProps(context) {
-  const endpoint = `https://${process.env.TRANSLATOR_TEXT_DOMAIN}.cognitiveservices.azure.com/translator/text/v3.0/translate?api-version=3.0&to=ta`;
-    const subscriptionKey = process.env.TRANSLATOR_TEXT_KEY;
-    const region = process.env.TRANSLATOR_TEXT_REGION;
+// export async function getServerSideProps(context) {
+//   const endpoint = `https://${process.env.TRANSLATOR_TEXT_DOMAIN}.cognitiveservices.azure.com/translator/text/v3.0/translate?api-version=3.0&to=ta`;
+//     const subscriptionKey = process.env.TRANSLATOR_TEXT_KEY;
+//     const region = process.env.TRANSLATOR_TEXT_REGION;
 
-    const headers = {
-          "Ocp-Apim-Subscription-Key": subscriptionKey,
-          "Ocp-Apim-Subscription-Region": region,
-          "Content-Type": "application/json",
-        };
+//     const headers = {
+//           "Ocp-Apim-Subscription-Key": subscriptionKey,
+//           "Ocp-Apim-Subscription-Region": region,
+//           "Content-Type": "application/json",
+//         };
 
-    const data = [{ Text: "Hello, what is your name?" }];
+//     const data = [{ Text: "Hello, what is your name?" }];
   
-    try {
-      const response = await axios.post(endpoint, data, { headers });
-      console.log(response.data[0].translations[0].text);
+//     try {
+//       const response = await axios.post(endpoint, data, { headers });
+//       console.log(response.data[0].translations[0].text);
       
 
 
-      // Process the translated text response here
-      return {
-        props: {
-          data: JSON.stringify(response.data[0].translations[0].text)
-        }
-      }
-    } catch (error) {
-      console.error(error);
-      // Handle the error
-    }
-}
+//       // Process the translated text response here
+//       return {
+//         props: {
+//           data: JSON.stringify(response.data[0].translations[0].text)
+//         }
+//       }
+//     } catch (error) {
+//       console.error(error);
+//       return{}
+//       // Handle the error
+//     }
+// }
