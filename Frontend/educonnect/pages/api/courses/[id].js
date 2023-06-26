d6d2f6886d09baf01aba0464b6f648a1c29132dd
Adithya R <rsadithya12@gmail.com>
2023-06-26 12:03:28 +0530

    backend for courses/id

--- Frontend/educonnect/pages/api/courses/[id].js
+++ Frontend/educonnect/pages/api/courses/[id].js
@@ -1,4 +1,22 @@
-export default function handler(req, res) {
-    res.status(200).json({ id: req.query.id })
+import supabase from "@/utils/supabaseClient"
+
+export default async function handler(req, res) {
+    const modules = await supabase.from('modules').select('id, module_name, module_no, topics, links, notes')
+    //res.status(200).json({ moduleList, moduleError })
+    const courseDetails = await supabase.from('courses').select('id, course_code, course_name').eq('id', req.query.id)
+    
+    if(modules.error || courseDetails.error) {
+        res.status(500).json({ error: modules.error || courseDetails.error })
+    }
+    else{
+        let resp = courseDetails.data[0]
+        // resp = {
+        //     ...resp,
+        //     modules: modules.data
+        // }
+        resp.modules = modules.data
+        res.status(200).json(resp)
+    }
+    
   }
   
